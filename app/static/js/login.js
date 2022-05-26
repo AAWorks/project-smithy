a5391c715ba711499aa97da24ed40abf801982d3
naronesty <noakai@gmail.com>
2022-05-26 09:03:21 -0400

    template fixes for usernames

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,76 +1,76 @@
-$("#signup").click(function() {
-    $("#first").fadeOut("fast", function() {
-    $("#second").fadeIn("fast");
-    });
-    });
+// $("#signup").click(function() {
+//     $("#first").fadeOut("fast", function() {
+//     $("#second").fadeIn("fast");
+//     });
+//     });
     
-    $("#signin").click(function() {
-    $("#second").fadeOut("fast", function() {
-    $("#first").fadeIn("fast");
-    });
-    });
+//     $("#signin").click(function() {
+//     $("#second").fadeOut("fast", function() {
+//     $("#first").fadeIn("fast");
+//     });
+//     });
     
     
       
-             $(function() {
-               $("form[name='login']").validate({
-                 rules: {
+//              $(function() {
+//                $("form[name='login']").validate({
+//                  rules: {
                    
-                   email: {
-                     required: true,
-                     email: true
-                   },
-                   password: {
-                     required: true,
+//                    email: {
+//                      required: true,
+//                      email: true
+//                    },
+//                    password: {
+//                      required: true,
                      
-                   }
-                 },
-                  messages: {
-                   email: "Please enter a valid email address",
+//                    }
+//                  },
+//                   messages: {
+//                    email: "Please enter a valid email address",
                   
-                   password: {
-                     required: "Please enter password",
+//                    password: {
+//                      required: "Please enter password",
                     
-                   }
+//                    }
                    
-                 },
-                 submitHandler: function(form) {
-                   form.submit();
-                 }
-               });
-             });
+//                  },
+//                  submitHandler: function(form) {
+//                    form.submit();
+//                  }
+//                });
+//              });
              
     
     
-    $(function() {
+//     $(function() {
       
-      $("form[name='registration']").validate({
-        rules: {
-          firstname: "required",
-          lastname: "required",
-          email: {
-            required: true,
-            email: true
-          },
-          password: {
-            required: true,
-            minlength: 5
-          }
-        },
+//       $("form[name='registration']").validate({
+//         rules: {
+//           firstname: "required",
+//           lastname: "required",
+//           email: {
+//             required: true,
+//             email: true
+//           },
+//           password: {
+//             required: true,
+//             minlength: 5
+//           }
+//         },
         
-        messages: {
-          firstname: "Please enter your firstname",
-          lastname: "Please enter your lastname",
-          password: {
-            required: "Please provide a password",
-            minlength: "Your password must be at least 5 characters long"
-          },
-          email: "Please enter a valid email address"
-        },
+//         messages: {
+//           firstname: "Please enter your firstname",
+//           lastname: "Please enter your lastname",
+//           password: {
+//             required: "Please provide a password",
+//             minlength: "Your password must be at least 5 characters long"
+//           },
+//           email: "Please enter a valid email address"
+//         },
       
-        submitHandler: function(form) {
-          form.submit();
-        }
-      });
-    });
+//         submitHandler: function(form) {
+//           form.submit();
+//         }
+//       });
+//     });
     
