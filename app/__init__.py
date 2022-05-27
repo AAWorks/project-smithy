bf6b265a93d0e832b73fd700d26ea50f7378aeca
naronesty <noakai@gmail.com>
2022-05-26 21:36:13 -0400

    commas

--- app/__init__.py
+++ app/__init__.py
@@ -118,7 +118,7 @@ def disp_home():
     ''' Loads the landing page '''
     try:
         if session:
-            return render_template("home.html", returning=session['user_id'])
+            return render_template("home.html", returning=", "+session['user_id'] + ",")
         else:
             return render_template("home.html")
     except:
