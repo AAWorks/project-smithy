659ff5537234f29a2e1a5cd2e173d3b37e5b71d4
zou-justin <justin82049@gmail.com>
2022-05-27 09:06:15 -0400

    adding editProfile

--- app/__init__.py
+++ app/__init__.py
@@ -107,6 +107,16 @@ def rAuthenticate():
                 else:
                     return render_template('register.html', taken=True)
 
+@app.route("/edit")
+def editProfile():
+    try:
+        if session:
+            return render_template("edit.html")
+        else:
+            return render_template("home.html")
+    except:
+        return render_template("error.html")
+
 @app.route("/logout")
 def logout():
     ''' Logout user by deleting user from session dict. Redirects to loginpage '''
