d78c45a52d203733b888777575cd8768631d16c6
Justin <justin82049@gmail.com>
2022-05-27 22:24:21 -0400

    updates

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,4 +1,22 @@
 function edit(){
-
+ var button = document.getElementById()
     
+}
+
+
+const editables = document.querySelectorAll("[contenteditable]");
+
+// save edits
+editables.forEach(el => {
+  el.addEventListener("blur", () => {
+    localStorage.setItem("dataStorage-" + el.id, el.innerHTML);
+  })
+});
+
+// once on load
+for (var key in localStorage) {
+  if (key.includes("dataStorage-")) {
+    const id = key.replace("dataStorage-","");
+    document.querySelector("#" + id).innerHTML = localStorage.getItem(key);
+  }
 }
