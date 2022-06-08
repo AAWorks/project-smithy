d0cda559cae3a0737a779fa514aa68a42e7e45d0
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2022-06-08 13:15:56 -0400

    dashboard addition in progress

--- app/static/js/datatables-simple-demo.js
+++ app/static/js/datatables-simple-demo.js
@@ -7,3 +7,13 @@ window.addEventListener('DOMContentLoaded', event => {
         new simpleDatatables.DataTable(datatablesSimple);
     }
 });
+
+window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+    // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+    const datatablesSimple2 = document.getElementById('datatablesSimple2');
+    if (datatablesSimple2) {
+        new simpleDatatables.DataTable(datatablesSimple2);
+    }
+});
