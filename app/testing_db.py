fb33aedfb9d1eb9e8c1a40eaa2bfa13b77d02afe
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2022-07-15 23:27:10 -0400

    minor change

--- app/testing_db.py
+++ app/testing_db.py
@@ -3,7 +3,10 @@ import itertools
 from db_funcs import *
 import db_builder
 
-if False:
+db = SqliteDb("project_reviewal.db")
+print(db.select("users"))
+
+"""if False:
     path = "var/www/app/"
 else:
     path= ""
@@ -17,4 +20,4 @@ combined = [x for x in itertools.chain.from_iterable(itertools.zip_longest(commo
 stuyusers = [(combined[i][0] + combined[i+1] + "20") for i in range(0, len(combined), 2)]
 
 for i in range(10):
-    create_user(stuyusers[i], "password", common_firsts[i], common_lasts[i], stuyusers[i], "static/images/users/default.png", "Student")
+    create_user(stuyusers[i], "password", common_firsts[i], common_lasts[i], stuyusers[i], "static/images/users/default.png", "Student")"""
